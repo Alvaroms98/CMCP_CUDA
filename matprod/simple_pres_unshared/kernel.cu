b95fdf10593396c753a055018526c10f6898e2bb
Alvaroms98 <alvaro.moure1998@gmail.com>
2021-10-27 19:57:12 +0200

    añadiendo un print en el kernel

--- matprod/simple_pres_unshared/kernel.cu
+++ matprod/simple_pres_unshared/kernel.cu
@@ -23,6 +23,7 @@ __global__ void vecAddKernelUnshared(float* A, float* B, float* C, int n) {
             Csub += A[i*n + k] * B[k*n + j]; 
         }
         C[i*n+j] = Csub;
+        printf("%f\n",C[i*n+j]);
     }
 }
 
