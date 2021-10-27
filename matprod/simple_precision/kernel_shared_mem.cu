60c3c825d69121dafc8fbc3ec5fce686e6d307f5
Alvaroms98 <alvaro.moure1998@gmail.com>
2021-10-27 10:36:46 +0200

    Para que el problema era la sensibilidad hacia las mayusculas Bs y BS

--- matprod/simple_precision/kernel_shared_mem.cu
+++ matprod/simple_precision/kernel_shared_mem.cu
@@ -13,7 +13,7 @@ __global__ void vecAddKernelShared(float* A, float* B, float* C, int n) {
     /* Definir identificadores */
     int bx = blockIdx.x, by = blockIdx.y;
     int tx = threadIdx.x, ty = threadIdx.y;
-    int BS = HILOS; // blockDim.x == blockDim.y
+    int BS = blockDim.x; // blockDim.x == blockDim.y
 
     /* Primer índice de las matrices de cada block (submatrices) */
 
@@ -31,12 +31,12 @@ __global__ void vecAddKernelShared(float* A, float* B, float* C, int n) {
        simultánea */
     for (int a=aBegin, b=bBegin; a<=aBegin + n - 1; a+=BS, b+=BS*n)
     {
-        __shared__ float As[BS][BS];
-        __shared__ float Bs[BS][BS];
+        __shared__ float Asub[BS][BS];
+        __shared__ float Bsub[BS][BS];
         // As[ty][tx] posición local de cada hilo dentro del bloque
         // las filas son las coordenadas "y" y las columnas las "x"
-        As[ty][tx] = A[a + n * ty + tx];//n*ty+tx == i*ld + j
-        Bs[ty][tx] = B[b + n * ty + tx];
+        Asub[ty][tx] = A[a + n * ty + tx];//n*ty+tx == i*ld + j
+        Bsub[ty][tx] = B[b + n * ty + tx];
 
         __syncthreads(); // Sincronización para asegurar carga completa
 
@@ -46,7 +46,7 @@ __global__ void vecAddKernelShared(float* A, float* B, float* C, int n) {
             realice el producto matricial que le corresponde */
         for (int k = 0; k < BS; k++)
         {
-            Csub += As[ty][k] * Bs[k][tx];
+            Csub += Asub[ty][k] * Bsub[k][tx];
         }
         __syncthreads(); // Sincronización para que no se altere la memoria
         // compartida de la siguiente iteración
